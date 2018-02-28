eda0123c3569d749642fd3044dfe657d1e9e2829
Partical777 <z25993021@gmail.com>
2018-03-01 01:54:36 +0800

    0.3.0
    Add land

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 //variable===============================================
+var landheight = 150;
+
 var canvaswidth = 600*2;
 var canvasheight = 480*1.5;
 var rectsize = 30;
 var rectx = 100;    //default x position
-var recty = canvasheight - rectsize;    //default y position
+var recty = canvasheight - rectsize - landheight;    //default y position
 var lineangle = 0;    //default angle
 var throwballsize = 35;
 
@@ -11,32 +13,67 @@ var movespeed = 2;
 var turnanglespeed = 1;
 var xspeedconst = 5;
 var yspeedconst = 5;
-var gravity = 0.1
+var gravity = 0.1;
+var powerbarspeed = 0.75;
 
 //const==================================================
 var powerpercent = 0;   //default percent
 var powerbool = true;
-var toward = 60;    //const aangle text position
+var toward = 60;    //const angle text position
 var cosxlineanglepower = 0;
 var sinylineanglepower = 0;
 var Xthrowblock = 0;
 var Ythrowblock = 0;
 var lockmovewhenthrowing = false;
 
+//matter.js
+// module aliases
+var Engine = Matter.Engine,
+    // Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var world;
+
+var ground;
 
 
 function setup() {
   createCanvas(canvaswidth, canvasheight);
-  angleMode(DEGREES)
+  angleMode(DEGREES);
+  
+  //matter
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  var options = {
+    isStatic: true
+  }
+  ground = Bodies.rectangle(200, canvasheight - landheight, canvaswidth, landheight, options);
+  World.add(world, ground);
 }
 
 function draw() {
   background(255, 180, 210);
   
+  //land
+  
+  push();
+  noStroke();
+  fill(color('#b8f1ed'));
+  beginShape();
+  vertex(0, canvasheight - landheight);  //left up
+  vertex(canvaswidth, canvasheight - landheight);  //right up
+  vertex(canvaswidth, canvasheight);  //right bottom
+  vertex(0, canvasheight);  //left bottom
+  
+  endShape(CLOSE);
+  pop();
+  
   //Position========================================
   
   rect(rectx, recty, rectsize, rectsize);
-  
   
   //Angle========================================
   
@@ -58,9 +95,9 @@ function draw() {
   
   //Power=========================================
   
-  rect(0, 0, powerpercent*(canvaswidth/100), 60);
+  rect(0, 0, ceil(powerpercent)*(canvaswidth/100), 60);
   textSize(32);
-  text(powerpercent, 30, 60);
+  text(ceil(powerpercent), 30, 60);
   textSize(18);
   text('0', 0, 30);
   text('1', 1*(canvaswidth/10), 30);
@@ -120,9 +157,9 @@ function draw() {
         powerpercent = 0;
       }
       if(powerbool){
-        powerpercent += 1;
+        powerpercent += powerbarspeed;
       }else{
-        powerpercent -= 1;
+        powerpercent -= powerbarspeed;
       }
     }
   }
@@ -134,8 +171,8 @@ function draw() {
 function keyReleased() {
   if(!lockmovewhenthrowing){    //if block is throwing, it can't action
     if (keyCode === 32) {
-      cosxlineanglepower = cos(lineangle)*powerpercent/xspeedconst;
-      sinylineanglepower = sin(lineangle)*powerpercent/yspeedconst;
+      cosxlineanglepower = cos(lineangle)*ceil(powerpercent)/xspeedconst;
+      sinylineanglepower = sin(lineangle)*ceil(powerpercent)/yspeedconst;
       Xthrowblock = rectx + (rectsize/2);
       Ythrowblock = recty + (rectsize/2);
       lockmovewhenthrowing = true;
@@ -155,6 +192,11 @@ function throwfunc(){
     sinylineanglepower -= gravity;
     ellipse(Xthrowblock, Ythrowblock, throwballsize, throwballsize);
   }
+  
+  if(Ythrowblock > canvasheight - landheight){
+      
+  }
+  
   if(Xthrowblock < 0 || Xthrowblock > canvaswidth || Ythrowblock > canvasheight){
     resetthrow(); 
   }
